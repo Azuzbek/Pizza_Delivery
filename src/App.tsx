8545811546439ae1861aa783552d81ced70edaa0
Aziz <kanalc@bk.ru>
2024-06-12 19:44:10 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import Button from './components/Button/Button'
+import Input from './components/Input/Input'
+
+function App() {
+	return (
+		<>
+			{/* <Button>button</Button>
+			<Button appearence='big'>button</Button>
+			<Input placeholder='Введите имя'></Input> */}
+		</>
+	)
+}
+
+export default App
